5356cbe229aa2342a5294998938c312567d1fb73
Dominic Bett <dbett49@gmail.com>
2024-03-25 22:57:00 +0300

    initial app setup

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from dotenv import load_dotenv, find_dotenv
 import streamlit as st
 
+
 def launch_app():
-  print('launching app')
-  st.title('Github PM Assistant')
- 
+    print('launching app')
+    st.title('Github Issues PM Assistant')
+
 
 if __name__ == "__main__":
-  launch_app();
+    load_dotenv(find_dotenv(), override=True)
+    launch_app()
