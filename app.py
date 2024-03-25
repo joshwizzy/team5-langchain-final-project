eb8c65ee7619048ad54308814c0071efbe3c822f
Dominic Bett <dbett49@gmail.com>
2024-03-25 20:28:03 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv, find_dotenv
+import streamlit as st
+
+def launch_app():
+  print('launching app')
+  st.title('Github PM Assistant')
+ 
+
+if __name__ == "__main__":
+  launch_app();
