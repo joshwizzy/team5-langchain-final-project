d4155358b55d14dd764c3f4d0c3d14c9b8bbb055
Joshua Masiko <joshuamasiko@gmail.com>
2024-03-27 16:54:26 +0300

    override system sqlite3

--- api/data/chroma/init.py
+++ api/data/chroma/init.py
@@ -3,6 +3,11 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 
 
+__import__("pysqlite3")
+import sys
+
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+
 load_dotenv(override=True)
 
 
